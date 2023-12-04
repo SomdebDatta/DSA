3639ef4093df4a6a3e19b7b01604d3c992ef9629
Somdeb Datta <somdeb.datta@siemens.com>
2023-12-04 15:36:30 +0530

    solved palindrome

--- node_practice/01-js/medium/palindrome.js
+++ node_practice/01-js/medium/palindrome.js
@@ -4,7 +4,24 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  str = str.toLowerCase();
+  str = str.replace(/\s[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+  // str = str.replace(/ /g, "");
+  console.log(str);
+  const len = str.length;
+  if (len<2){
+    return true;
+  }
+  let n = len-1;
+  for (i=0; i<len/2; i++){
+    if(str[i] != str[n]){
+      return false
+    }
+    else{
+      n = n-1;
+    }
+    return true
+  }
 }
-
+console.log(isPalindrome("awa!"))
 module.exports = isPalindrome;
--- node_practice/rough.js
+++ node_practice/rough.js
@@ -1,3 +1,4 @@
-allVowels = 'aeiou';
-letter = 'x';
-console.log(allVowels.includes(letter));
+let mystr = "hollebye \\[";
+console.log(mystr + " " + mystr.length)
+replaced_str = mystr.replace(/[hello\s]/g, "")
+console.log(replaced_str + " " + replaced_str.length);
