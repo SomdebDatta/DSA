36a205203bb3a2fba9256ae47e8c77c2b948b247
SomdebDatta <somdebdatta@gmail.com>
2024-02-27 23:42:36 +0530

    selection sort

---
+++ leetcode/sorting/selection_sort.py
@@ -0,0 +1,22 @@
+"""
+Selection Sort:
+    1. In each iteration, we will select the minimum element from the range of the unsorted array using an inner loop.
+    2. After that, we will swap the minimum element with the first element of the selected range(in step 1). 
+    3. Finally, after each iteration, we will find that the array is sorted up to the first index of the range. 
+"""
+
+arr = [13, 46, 24, 52, 20, 9]
+arr2 = [1, 2, 3, 4]
+
+
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+print(selectionSort(arr2))
