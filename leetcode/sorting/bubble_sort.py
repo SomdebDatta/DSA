daecfe4ffc1a8dc994b812a6dd4868be39bce6e4
SomdebDatta <somdebdatta@gmail.com>
2024-02-28 00:05:48 +0530

    bubble sort

---
+++ leetcode/sorting/bubble_sort.py
@@ -0,0 +1,17 @@
+arr = [13, 46, 24, 52, 20, 9]
+arr2 = [1, 2, 3, 4]
+
+
+def bubbleSort(arr):
+
+    for i in range(len(arr)):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] < arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        print(arr)
+
+    return arr
+
+
+print(bubbleSort(arr))
+print(bubbleSort(arr2))
--- leetcode/sorting/selection_sort.py
+++ leetcode/sorting/selection_sort.py
@@ -3,6 +3,9 @@ Selection Sort:
     1. In each iteration, we will select the minimum element from the range of the unsorted array using an inner loop.
     2. After that, we will swap the minimum element with the first element of the selected range(in step 1). 
     3. Finally, after each iteration, we will find that the array is sorted up to the first index of the range. 
+
+    Time Complexity: O(N^2)
+    Space Complexity: 0(1)
 """
 
 arr = [13, 46, 24, 52, 20, 9]
