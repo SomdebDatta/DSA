04b587e9083c4444d4d394eb3c7809ae723ffb34
SomdebDatta <somdebdatta@gmail.com>
2024-02-28 10:25:26 +0530

    correct insertion sort

--- leetcode/sorting/insertion_sort.py
+++ leetcode/sorting/insertion_sort.py
@@ -3,19 +3,27 @@ Time complexity:
 Space complexity:
 """
 
+empty_arr = []
+single_elem_arr = [0]
 arr = [13, 46, 24, 52, 20, 9]
-arr2 = [1, 2, 3, 4]
+sorted_arr_asc = [1, 2, 3, 4]
+sorted_arr_desc = [4, 3, 2, 1]
 
 
 def insertionSort(arr):
-    for i in range(len(arr) - 1):
-        pos = i + 1
-        for j in range(0, i):
-            if arr[j] > arr[pos]:
-                arr[j], arr[pos] = arr[pos], arr[j]
 
+    for i in range(1, len(arr)):
+        current = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > current:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = current
     return arr
 
 
+print(insertionSort(empty_arr))
+print(insertionSort(single_elem_arr))
 print(insertionSort(arr))
-print(insertionSort(arr2))
+print(insertionSort(sorted_arr_asc))
+print(insertionSort(sorted_arr_desc))
