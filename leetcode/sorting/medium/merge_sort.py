5eca2e011978b60d5affc883773e36111ecb99df
SomdebDatta <somdebdatta@gmail.com>
2024-03-01 00:57:08 +0530

    corrected code for merge sort

--- leetcode/sorting/medium/merge_sort.py
+++ leetcode/sorting/medium/merge_sort.py
@@ -1,15 +1,19 @@
-arr = [0, -1, 10, 19, -5, 90]
+"""
+Merge sort uses divide and conquer approach.
+Remember 2 way merge sort, wherein, there are 2 input arrays, and elements are compared b/n the 2 arrays one by one
+and assembled into a single array.
+Similarly, in merge sort algorithm used below, the unsorted input array is broken into individual elements.
+Each element treated as a separate array and compare with the neighbouring element (now an array).
+These comparisons are then assembled into a single array.
+"""
+
+arr = [4, 2, 1, 6, 7]
 print(f"OG arr - {arr}")
 
 
 def merge(lb, ub):
-    import pdb
-
-    pdb.set_trace()
-    print(f"Inside merge func - lb - {lb} ub - {ub}")
     sorted_arr = list()
     mid = (lb + ub) // 2
-    print(f"mid - {mid}")
     i, j = lb, mid + 1
     while i <= lb and j <= ub:
         if arr[i] <= arr[j]:
@@ -26,8 +30,6 @@ def merge(lb, ub):
         j += 1
     print(f"sorted array - {sorted_arr}")
     arr[lb : ub + 1] = sorted_arr
-    # for i in range(lb, ub):
-    #     arr[i]
 
 
 def mergeSort(lb, ub):
@@ -35,11 +37,10 @@ def mergeSort(lb, ub):
         return
     if lb < ub:
         mid = (lb + ub) // 2
-        # print(f"lb - {lb} mid - {mid} ub - {ub} ")
         mergeSort(lb, mid)
         mergeSort(mid + 1, ub)
         merge(lb, ub)
 
 
-mergeSort(0, len(arr))
+mergeSort(0, len(arr) - 1)
 print(arr)
