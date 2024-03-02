95a2dd8bdb8f465a73896e233c13ef4117fa15cb
SomdebDatta <somdebdatta@gmail.com>
2024-03-02 23:37:18 +0530

    recursive insertion sort draft2

--- leetcode/sorting/medium/recursive_insertion_sort.py
+++ leetcode/sorting/medium/recursive_insertion_sort.py
@@ -6,26 +6,33 @@ sorted_arr_desc = [4, 3, 2, 1]
 
 
 def recInsertionSort(inp_arr):
+    i = 1
+    j = i - 1
+    current = inp_arr[i]
 
-    for i in range(1, len(inp_arr)):
-        current = inp_arr[i]
-        j = i - 1
+    def recursion(i, j, current):
+        # import pdb
 
-        def recursion(j):
-            # import pdb
-
-            # pdb.set_trace()
-            if j < 0 or inp_arr[j] < current:
-                return j
-            if inp_arr[j] > current:
-                inp_arr[j + 1] = inp_arr[j]
+        # pdb.set_trace()
+        if i == len(inp_arr):
+            return
+        if j < 0:
+            i += 1
+            j = i - 1
+            recursion(i, j, current)
+        if inp_arr[j] > current and j != len(inp_arr) - 1:
+            print(i, j)
+            inp_arr[j + 1] = inp_arr[j]
             j -= 1
-            recursion(j)
-
-        j = recursion(j) or j
+            recursion(i, j, current)
         inp_arr[j + 1] = current
+        i += 1
+        j = i - 1
+        current = inp_arr[i]
+        recursion(i, j, current)
 
-    return inp_arr
+    recursion(i, j, current)
+    print(inp_arr)
 
 
 print(recInsertionSort(arr))
