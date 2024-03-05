f1b9143813a48a30bcebb1ab76efd930f9f7035c
SomdebDatta <somdebdatta@gmail.com>
2024-03-05 11:38:38 +0530

    testing with duplicates - optimal solution

--- leetcode/Array-String/find_second_largest.py
+++ leetcode/Array-String/find_second_largest.py
@@ -21,4 +21,5 @@ def secondLargest(arr):
 
 arr = [4, 2, 1, 6, 7]
 arr2 = [4, 5, 3, 6, 7]
-secondLargest(arr2)
+arr_duplicate = [1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, -1]
+secondLargest(arr_duplicate)
