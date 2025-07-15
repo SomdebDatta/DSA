3b24ab376671114136f9d7a48a7bee7cbbe14f67
SomdebDatta <somdebdatta@gmail.com>
2025-07-15 13:05:18 +0530

    inheritance update

--- oop_python/Inheritance.py
+++ oop_python/Inheritance.py
@@ -11,26 +11,35 @@ def inheritance_mro():
             print('Inside A\'s init')
 
         def feature1(self):
-            print('feature1 working...')
+            print('feature1-A working...')
         
     class B:
         def __init__(self):
             print('Inside B\'s init')
+        
+        def feature1(self):
+            print('feature1-B working...')
 
         def feature2(self):
-            print('feature2 working')
+            print('feature2-B working')
 
-    class C(A, B):
+    class C(A,B):
         def __init__(self):
             super().__init__()
             print('Inside C\'s init')
+        
+        def feature1(self):
+            print('feature1-C working...')
+
         def feature3(self):
-            print('Feature3 working')
+            print('Feature3-C working')
         
         def feat(self):
             super().feature1()
 
     c = C()
+    c.feature1()
     c.feat()
 
-inheritance_mro()
+if __name__ == '__main__':
+    inheritance_mro()
